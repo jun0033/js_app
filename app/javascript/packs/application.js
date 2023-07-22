43fa703280ca56b0c25dee012d0ec4e3a4299e8c
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-07-22 09:35:02 +0000

    css設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,24 +2,23 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require rails-ujs
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import jQuery from "jquery";
+import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"
-import '@fortawesome/fontawesome-free/js/all'
+import "../stylesheets/application";
 
-import 'animate.css';
 import "jquery.inview.js";
 import "sample.js";
 
-global.$ = jQuery;
-window.$ = jQuery;
+
 
 Rails.start()
 Turbolinks.start()
