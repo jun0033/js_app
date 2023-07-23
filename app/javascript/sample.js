02551be16030ac4ebd38c0d53d17b232e10c3f15
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-07-23 06:57:14 +0000

    fadeintrigger

--- app/javascript/sample.js
+++ app/javascript/sample.js
@@ -21,3 +21,28 @@ function fadeAnime(){
   $(window).on('load', function(){
     fadeAnime();/* アニメーション用の関数を呼ぶ*/
   });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+//スクロールをするたびにアニメーションを行う設定  
+$('.fadeInUpTrigger').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInUp');//クラス名が付与
+    } else {//表示領域から出た時
+      $(this).removeClass('animate__animated animate__fadeInUp');//クラス名が除去
+    }
+  });
+
+//スクロールをしたら1度だけアニメーションをする設定
+  $('.fadeInUpTriggerOnce').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInUp');//クラス名が付与
+    }
+  });
+
+//違う動きを追加設定  
+$('.fadeInDownTrigger').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInDown');//クラス名が付与
+    } else {//表示領域から出た時
+      $(this).removeClass('animate__animated animate__fadeInDown');//クラス名が除去
+    }
+  });
