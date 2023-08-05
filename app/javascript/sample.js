c9279b5d04410100188d837c2c1176505e3e93ba
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-08-05 12:28:21 +0000

    CountUp

--- app/javascript/sample.js
+++ app/javascript/sample.js
@@ -83,3 +83,75 @@ tippy('.cap', {//指定した要素にツールチップが出現
   duration: 200,//ツールチップの出現の速さをミリ秒単位で指定
 }
 )
+
+$('#box1').on('inview', function(event, isInView) {
+  if (isInView) {
+    //要素が見えたときに実行する処理
+    $("#box1 .count-up").each(function(){
+      $(this).prop('Counter',0).animate({//0からカウントアップ
+            Counter: $(this).text()
+        }, {
+        // スピードやアニメーションの設定
+            duration: 2000,//数字が大きいほど変化のスピードが遅くなる。2000=2秒
+            easing: 'swing',//動きの種類。他にもlinearなど設定可能
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+  }
+});
+
+$('#box2').on('inview', function(event, isInView) {
+  if (isInView) {
+    //要素が見えたときに実行する処理
+    $("#box2 .count-down").each(function(){
+       $(this).prop('Counter',10).animate({//10からカウントダウン
+            Counter: $(this).text()
+        }, {
+        // スピードやアニメーションの設定
+            duration: 1000,//数字が大きいほど変化のスピードが遅くなる。1000=1秒
+            easing: 'swing',//動きの種類。他にもlinearなど設定可能
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+  }
+});
+
+$('#box3').on('inview', function(event, isInView) {
+  if (isInView) {
+    //要素が見えたときに実行する処理
+    $("#box3 .count-up").each(function(){
+      $(this).prop('Counter',0).animate({//0からカウントアップ
+            Counter: $(this).text()
+        }, {
+        // スピードやアニメーションの設定
+            duration: 2000,//数字が大きいほど変化のスピードが遅くなる。2000=2秒
+            easing: 'swing',//動きの種類。他にもlinearなど設定可能
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+  }
+});
+
+$('#box4').on('inview', function(event, isInView) {
+  if (isInView) {
+    //要素が見えたときに実行する処理
+    $("#box4 .count-up").each(function(){
+      $(this).prop('Counter',0).animate({//0からカウントアップ
+            Counter: $(this).text()
+        }, {
+        // スピードやアニメーションの設定
+            duration: 2000,//数字が大きいほど変化のスピードが遅くなる。2000=2秒
+            easing: 'swing',//動きの種類。他にもlinearなど設定可能
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+  }
+});
