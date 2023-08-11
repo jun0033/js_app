7e2c131707bded5d6c9ab263a25535485926fb2b
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-08-11 10:06:26 +0000

    splash

--- app/javascript/sample.js
+++ app/javascript/sample.js
@@ -180,3 +180,8 @@ $(window).on('load', function(){
     $(Box).slideDown(500);          //アコーディオンを開く
   });
 });
+
+$(window).on('load',function(){
+  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+});
